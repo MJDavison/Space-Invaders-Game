d3457ed01afaa2110702eed0da29b9fa2ad56657
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-12-26 00:19:20 +0000

    Fixed some issues then added a player controller.
    For some reason, the project wouldn't play well and refused to accept that I had the "UnityEngine.InputSystem" added... so I exported everything I'd done so far into a package, deleted the project (after making a backup) and made a new one... everything then worked fine.
    
    Afterwards I made a player controller and implemented movement and shooting.

---
+++ Space-Invaders-Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+namespace MJDeveloping.Unity.Games.Space_Invader_Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [ReadOnly][SerializeField] Rigidbody2D myRB;
+        [ReadOnly][SerializeField] GameManager myGM;
+
+        [SerializeField] float playerMovementSpeed;
+        [SerializeField] GameObject projectilePrefab;
+        [ReadOnly,SerializeField] float shotPosition = 0.4f;
+        // Start is called before the first frame update
+        void Start()
+        {
+            myRB = GetComponent<Rigidbody2D>();
+            myGM = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnMove(InputValue value){
+            float horizontalInput = value.Get<float>();
+            
+            Debug.Log("Move " + horizontalInput);
+            myRB.velocity = Vector2.right * horizontalInput * playerMovementSpeed;
+        }
+
+        public void OnFire(){
+            Debug.Log("Fire");
+            GameObject projectile = Instantiate(projectilePrefab,new Vector2(transform.position.x, transform.position.y + shotPosition),transform.rotation);
+            projectile.GetComponent<Rigidbody2D>().velocity = Vector2.up * 5;
+        }
+    }
+}
